7d7dce975be1520612441fa5c9d543f555c05224
kkksohee <thgml0716030@naver.com>
2025-06-09 23:03:15 +0900

    6월 9일

---
+++ work_backend/example-jpa-0605/src/main/java/com/hd/example_jpa_0605/ExampleJpa0605Application.java
@@ -0,0 +1,13 @@
+package com.hd.example_jpa_0605;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJpa0605Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJpa0605Application.class, args);
+	}
+
+}
---
+++ work_backend/example-jpa-0605/src/test/java/com/hd/example_jpa_0605/ExampleJpa0605ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.example_jpa_0605;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJpa0605ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
